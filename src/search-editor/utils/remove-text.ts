5256dbfcd1056ef173ebbfd13da7ee2646527b92
Oluwaseun Jolaoso <sudhons@gmail.com>
2025-05-13 10:24:09 +0100

    Remove trailing spaces from suggest inputs

--- src/search-editor/utils/remove-text.ts
+++ src/search-editor/utils/remove-text.ts
@@ -5,16 +5,16 @@ function escapeRegExp(text: string): string {
 }
 
 export function removeExtraSpaces(input: string) {
-	return input.replace(/\s+/g, ' ');
+	return input.replace(/\s+/g, ' ').trim();
 }
 
 export function removeTextFromEnd(input: string, textToRemove: string): string {
-	return input.replace(new RegExp(`${escapeRegExp(textToRemove)}$`), '');
+	return input.replace(new RegExp(`${escapeRegExp(textToRemove)}$`), '').trim();
 }
 
 export function removeTextFromFront(
 	input: string,
 	textToRemove: string,
 ): string {
-	return input.replace(new RegExp(`^${escapeRegExp(textToRemove)}`), '');
+	return input.replace(new RegExp(`^${escapeRegExp(textToRemove)}`), '').trim();
 }
